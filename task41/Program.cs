87932f193a770135531add8e3a73361880faf517
Dariya Popova <dariyp26@mail.ru>
2022-09-07 16:13:59 +0300

    ДЗ к семинару 6

---
+++ task41/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4 */
+
+Console.WriteLine("Введите несколько чисел в формате 1, -7, 567, 89, 223");
+string str = Console.ReadLine() +',';
+string num = "";
+int calc = 0;
+int number = 0;
+
+for (int i = 0; i < str.Length; i++)
+{
+  if (str[i] != ' '){
+  if (str[i] != ',')
+  {
+    num += str[i];
+  }
+  else
+  {
+    number = int.Parse(num);
+    if (number > 0)
+    {
+      calc ++;
+    }
+    num = "";
+  }
+  }
+}
+
+Console.WriteLine($"Чисел больше нуля: {calc}.");
---
+++ task43/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.WriteLine("Yравнения y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine("b1 = ?");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("k1 = ?");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("b2 = ?");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("k2 = ?");
+double k2 = double.Parse(Console.ReadLine());
+
+double x = 0;
+double y = 0;
+
+if (k1 == k2)
+{
+  Console.WriteLine("Прямые паралельны или совпадают.");
+}
+else
+{
+  x = (b2-b1)/(k1-k2);
+y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения двух прямых ({x}; {y})");
+}
